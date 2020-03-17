cec0d2f690d7ed60b802f44012d064eab2f186bc
ellapark <ellapark@labs.local>
2020-03-17 14:15:22 +0900

    Initial commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws"{
+    access_key = "AKIAQS32EJVZJBVLDJES"
+    secret_key = "yDr2n9feIHqjUSzPzb5oKSP+xCbkfCMVt4BjXc8v"
+    region = "us-west-2"
+}
+#resource "aws_instance" "example" {
+#  ami = "ami-02d0ea44ae3fe9561"
+#  instance_type = "t2.micro"
+#}
+
